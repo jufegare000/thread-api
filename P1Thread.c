c93458ab2d978819534d6db0f10a8cf4002a3504
Juan Gallo <jufegare@gmail.com>
2020-04-18 12:49:10 -0500

    punto 1

--- P1Thread.c
+++ P1Thread.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <time.h>
 
-void* factorial(void* arg);
+void *factorial(void *arg);
 
 int main(int argc, char *argv[])
 {
@@ -12,17 +12,19 @@ int main(int argc, char *argv[])
 
     pthread_t id1;
     pthread_t id2;
-    int *val1 = atoi(argv[1]);
-    int *val2 = atoi(argv[2]);
+    int val1 = atoi(argv[1]);
+    int val2 = atoi(argv[2]);
+    long long int *res1, *res2;
     pthread_create(&id1, NULL, &factorial, &val1);
     pthread_create(&id2, NULL, &factorial, &val2);
-    //printf("El factorial de %s es %d\n", argv[1], factorial(atoi(argv[1])));
-    //printf("El factorial de %s es %d\n", argv[2], factorial(atoi(argv[2])));
-
+    pthread_join(id1, (void *)&res1);
+    pthread_join(id2, (void *)&res2);
+    printf("Factorial de: %d es: %lld \n", val1, res1);
+    printf("Factorial de: %d es: %lld \n", val2, res2);
     return 0;
 }
 
-void* factorial(void* arg)
+void *factorial(void *arg)
 {
     int n = *((int *)arg);
     int resultado = 1;
